9cdf59273bc8a00702f799b47b84c4ebe6e9607e
Reid Wilbur <reid.wilbur@gmail.com>
2013-08-04 18:58:45 -0500

    updating to work with all list cases

--- 99probs/p08.scala
+++ 99probs/p08.scala
@@ -4,6 +4,9 @@ def compress(list: List[Any]): List[Any] = {
     if (list == Nil) {
       compList.reverse
     }
+    else if (compList == Nil) {
+      compressTR(list.head +: compList, list.tail)
+    }
     else if (compList.head == list.head) {
       compressTR(compList, list.tail)
     }
@@ -12,9 +15,12 @@ def compress(list: List[Any]): List[Any] = {
     }
   }
 
-  compressTR(list.head +: Nil, list.tail)
+  compressTR(Nil, list)
 }
 
 val l = compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
 println(l)
 assert(List('a, 'b, 'c, 'a, 'd, 'e) == l)
+
+assert(compress(Nil) == Nil)
+assert(compress(List('b)) == List('b))
