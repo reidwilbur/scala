9e4e2bfbd1ef957c2b39badf7abb9f2d36d733fd
Reid Wilbur <reid.wilbur@gmail.com>
2013-09-08 22:14:02 -0500

    working on p55 test

--- 99probs/src/test/scala/p55test.scala
+++ 99probs/src/test/scala/p55test.scala
@@ -6,9 +6,69 @@ package bintree {
   class p50test extends FunSuite {
     import bintree._
 
-    test("") {
+    test("Tree.cBalanced(1, 'x) == List(Tree('x))") {
+      val trees = Tree.cBalanced(1, 'x)
+
+      val exp = List(Node('x))
+
+      assert(trees == exp)
     }
 
+    test("Tree.cBalanced(2, 'x)") {
+      val trees = Tree.cBalanced(2, 'x)
+
+      val exp = List(
+        Node('x, Node('x, End, End), End), 
+        Node('x, End, Node('x, End, End))
+      )
+
+      assert(exp.length == trees.length)
+      exp foreach { assert( trees.contains _ ) }
+    }
+
+    test("Tree.cBalanced(3, 'x)") {
+      val trees = Tree.cBalanced(3, 'x)
+
+      val exp = List(Node('x, Node('x, End, End), Node('x, End, End)))
+
+      assert(exp.length == trees.length)
+      exp foreach { assert( trees.contains _ ) }
+    }
+
+    test("Tree.cBalanced(4, 'x)") {
+      val trees = Tree.cBalanced(4, 'x)
+
+      val exp = List(
+        Node('x, Node('x, Node('x, End, End), End), Node('x, End, End)),
+        Node('x, Node('x, End, Node('x, End, End)), Node('x, End, End)),
+        Node('x, Node('x, End, End), Node('x, Node('x, End, End), End)),
+        Node('x, Node('x, End, End), Node('x, End, Node('x, End, End))),
+      )
+
+      assert(exp.length == trees.length)
+      exp foreach { assert( trees.contains _ ) }
+    }
+
+    test("Tree.cBalanced(5, 'x)") {
+      val trees = Tree.cBalanced(5, 'x)
+
+      val exp = List(
+        Node('x, Node('x, Node('x, End, End), Node('x, End, End)), Node('x, End,                End)),
+
+        Node('x, Node('x, Node('x, End, End), End),                Node('x, Node('x, End, End), End)),
+        Node('x, Node('x, Node('x, End, End), End),                Node('x, End,                Node('x, End, End))),
+        
+        Node('x, Node('x, End,                Node('x, End, End)), Node('x, Node('x, End, End), End)),
+        Node('x, Node('x, End,                Node('x, End, End)), Node('x, End,                Node('x, End, End))),
+
+        Node('x, Node('x, End, Node('x, End, End)), Node('x, End, End)),
+        Node('x, Node('x, End, End), Node('x, Node('x, End, End), End)),
+        Node('x, Node('x, End, End), Node('x, End, Node('x, End, End))),
+      )
+
+      assert(exp.length == trees.length)
+      exp foreach { assert( trees.contains _ ) }
+    }
   }
 
 }
