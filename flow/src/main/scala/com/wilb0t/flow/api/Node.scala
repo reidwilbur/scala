6315325e2dc3ae3efc90339d8b91af89cca5c4d6
Reid Wilbur <reid.wilbur@amd.com>
2013-10-11 18:02:31 -0500

    refactoring to create scala native flow construction syntax

--- flow/src/main/scala/com/wilb0t/flow/api/Node.scala
+++ flow/src/main/scala/com/wilb0t/flow/api/Node.scala
@@ -3,19 +3,43 @@ package com.wilb0t.flow.api
 import com.weiglewilczek.slf4s.Logging
 
 abstract class ExitPort {
-  def name: String
   def description: String
+  override def toString: String = {
+    this.getClass().getSimpleName()+":"+description
+  }
 }
 
 trait Action {
   def execute: ExitPort
 }
 
-class FlowNode(val name: String, val nodeAction: Action, val nextNodes: Map[ExitPort, FlowNode]) {
-  def nextNode(exitPort: ExitPort): Option[FlowNode] = nextNodes.get(exitPort)
+class FlowNode(val name: String, val action: Action, val exitPorts: Map[ExitPort, String]) extends Logging {
+
+  def execute: (ExitPort, Option[String]) = {
+    logger.info("Executing node "+name)
+    val exitPort = action.execute
+    val nextNode = exitPorts(exitPort)
+    (exitPort, Some(nextNode))
+  }
+
+  override def toString: String = {
+    name
+  }
 }
 
-class Flow(val head: FlowNode) extends Logging {
+class EndFlowNode(name: String, action: Action) extends FlowNode(name, action, Map.empty) {
+  override def execute: (ExitPort, Option[String]) = {
+    logger.info("Executing node "+name)
+    val exitPort = action.execute
+    (exitPort, None)
+  }
+}
+
+class Flow(name: String, val nodes: List[FlowNode]) extends Logging {
+
+  val nodeMap: Map[String, FlowNode] = 
+    nodes.foldLeft(Map[String, FlowNode]())( (m, n) => m + (n.name -> n))
+
   def execute: List[(FlowNode, ExitPort)] = {
     def execNode(node: Option[FlowNode], path: List[(FlowNode, ExitPort)]): List[(FlowNode, ExitPort)] = {
       node match {
@@ -23,16 +47,21 @@ class Flow(val head: FlowNode) extends Logging {
           logger.info("Flow finished")
           path.reverse
         case Some(n) =>
-          logger.info("Executing node: "+n.name)
-          val exitPort = n.execute
-          logger.info("Got exit port: "+exitPort.name+" - "+exitPort.description)
-          val nextNode = n.nextNode(exitPort)
+          val (exitPort, nextNodeName) = n.execute
+          logger.info("Got exit port: "+exitPort)
+          logger.info("Next node: "+nextNodeName)
+          val nextNode = 
+            nextNodeName match {
+              case Some(name) => nodeMap.get(name)
+              case _ => None
+            }
           execNode(nextNode, (n, exitPort) :: path)
       }
 
     }
-     
-    execNode(Some(head), Nil)
+    
+    logger.info("Starting flow")
+    execNode(nodes.headOption, Nil)
   }
 
 }
--- flow/src/test/scala/com/wilb0t/flow/api/NodeTest.scala
+++ flow/src/test/scala/com/wilb0t/flow/api/NodeTest.scala
@@ -4,50 +4,47 @@ import org.scalatest.FunSuite
 import com.weiglewilczek.slf4s.Logging
 
 case class PassExit() extends ExitPort {
-  val name = "PassExit"
   val description = "Node Passed"
 }
 
 case class FailExit() extends ExitPort {
-  val name = "FailExit"
   val description = "Node Failed"
 }
 
-class FailNode(val name: String) extends Executable with Logging {
+class PassAction extends Action with Logging {
   override def execute: ExitPort = {
-    logger.info(name+" Executing")
-
-    FailExit()
-  }
-
-  override def toString: String = {
-    this.getClass().getSimpleName()+":"+name
-  }
-}
-
-class PassNode(val name: String) extends FlowNode with Logging {
-  override def execute: ExitPort = {
-    logger.info(name+" Executing")
+    logger.info("Emitting PassExit")
 
     PassExit()
   }
+}
 
-  override def nextNode(exitPort: ExitPort): Option[FlowNode] = {
-    exitPort match {
-      case PassExit() => new Some(new FailNode("FailNode1"))
-      case _ => None
-    }
-  }
+class FailAction extends Action with Logging {
+  override def execute: ExitPort = {
+    logger.info("Emitting FailExit")
 
-  override def toString: String = {
-    this.getClass().getSimpleName()+":"+name
+    FailExit()
   }
 }
 
 class FlowTest extends FunSuite with Logging {
+
   test("simple flow") {
-    val head = new PassNode("PassNode1")
-    val flow = new Flow(head)
+
+    val flow = 
+      new Flow("TestFlow", List[FlowNode](
+            new FlowNode("HeadNode", new PassAction(), Map[ExitPort, String](
+              ( PassExit() -> "MidNode" ),
+              ( FailExit() -> "EndNode" )
+            )),
+            new FlowNode("MidNode", new PassAction(), Map[ExitPort, String](
+              ( PassExit() -> "EndNode" ),
+              ( FailExit() -> "EndNode" )
+            )),
+            new EndFlowNode("EndNode", new FailAction())
+            )
+          )
+
 
     val path = flow.execute
     logger.info("Got path: "+path)
