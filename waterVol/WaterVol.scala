052eb9e19f7a2bd665a2f39bf67f4621de3ad148
Reid Wilbur <reid.wilbur@gmail.com>
2013-11-01 19:50:43 -0500

    Updated to solution to use single scan of input heights

--- waterVol/WaterVol.scala
+++ waterVol/WaterVol.scala
@@ -1,42 +1,46 @@
+import scala.annotation.tailrec
 
-def waterVol(heights: List[Int]): Int = {
-  def getLocalMax(heights: List[Int], curMax: Int, maxHeights: List[Int]): List[Int] = {
-    heights match {
-      case Nil => maxHeights.reverse
-      case h :: t => 
-        val nextMax = if (h > curMax) h else curMax
-        getLocalMax(t, nextMax, (nextMax :: maxHeights))
+def waterVol(heights: Vector[Int]): Int = {
+  @tailrec
+  def calcVol(l: Int, r: Int, curLvl: Int, curVol: Int): Int = {
+    if (l == r)
+      curVol
+    else {
+      if (heights(l) < heights(r)) {
+        val newLvl = if (heights(l) > curLvl) heights(l) else curLvl
+        val newVol = curVol + (newLvl - heights(l))
+        calcVol(l+1, r, newLvl, newVol)
+      }
+      else {
+        val newLvl = if (heights(r) > curLvl) heights(r) else curLvl
+        val newVol = curVol + (newLvl - heights(r))
+        calcVol(l, r-1, newLvl, newVol)
+      }
     }
   }
 
-  val lMax = getLocalMax(heights, 0, Nil)
-  val rMax = getLocalMax(heights.reverse, 0, Nil).reverse
-
-  val maxLvl = lMax.zip(rMax).map{ case (l,r) => math.min(l,r) }
-
-  val waterHeights = maxLvl.zip(heights).map{ case (m, h) => m - h }
-  waterHeights.foldLeft(0)( (vol, h) => vol+h )
+  calcVol(0, heights.length-1, 0, 0)
 }
 
-assert(waterVol(List(2, 5, 1, 2, 3, 4, 7, 7, 6)) == 10)
-assert(waterVol(List(2, 5, 1, 3, 1, 2, 1, 7, 7, 6)) == 17)
-assert(waterVol(List(2, 3, 1, 2, 3, 1, 3)) == 5)
-assert(waterVol(List(1, 2, 3, 4, 5, 6, 7, 8, 9)) == 0)
-assert(waterVol(List(9, 8, 7, 6, 5, 4, 3, 2, 1)) == 0)
-assert(waterVol(List(1, 1, 1, 1, 1)) == 0)
-assert(waterVol(List(1, 0, 1)) == 1)
-assert(waterVol(List(5, 0, 5)) == 5)
-assert(waterVol(List(5, 0, 4)) == 4)
-assert(waterVol(List(4, 0, 5)) == 4)
-assert(waterVol(List(4, 0, 5, 0, 2)) == 6)
-assert(waterVol(List(0, 1, 0, 1, 0)) == 1)
-assert(waterVol(List(0, 1, 0, 0, 1, 0)) == 2)
-assert(waterVol(List(4, 2, 2, 1, 1, 1, 3)) == 8)
-assert(waterVol(List(0, 3, 2, 1, 4)) == 3)
-assert(waterVol(List(1, 0, 1, 0)) == 1)
-assert(waterVol(List(1, 0, 1, 2, 0, 2)) == 3)
-assert(waterVol(List(2, 5, 1, 2, 3, 4, 7, 7, 6)) == 10)
-assert(waterVol(List(5, 1, 0, 1)) == 1)
-assert(waterVol(List(2, 5, 1, 2, 3, 4, 7, 7, 6, 3, 5)) == 12)
-assert(waterVol(List(3, 0, 1, 0, 2)) == 5)
+assert(waterVol(Vector(2, 5, 1, 2, 3, 4, 7, 7, 6)) == 10)
+assert(waterVol(Vector(2, 5, 1, 3, 1, 2, 1, 7, 7, 6)) == 17)
+assert(waterVol(Vector(2, 3, 1, 2, 3, 1, 3)) == 5)
+assert(waterVol(Vector(1, 2, 3, 4, 5, 6, 7, 8, 9)) == 0)
+assert(waterVol(Vector(9, 8, 7, 6, 5, 4, 3, 2, 1)) == 0)
+assert(waterVol(Vector(1, 1, 1, 1, 1)) == 0)
+assert(waterVol(Vector(1, 0, 1)) == 1)
+assert(waterVol(Vector(5, 0, 5)) == 5)
+assert(waterVol(Vector(5, 0, 4)) == 4)
+assert(waterVol(Vector(4, 0, 5)) == 4)
+assert(waterVol(Vector(4, 0, 5, 0, 2)) == 6)
+assert(waterVol(Vector(0, 1, 0, 1, 0)) == 1)
+assert(waterVol(Vector(0, 1, 0, 0, 1, 0)) == 2)
+assert(waterVol(Vector(4, 2, 2, 1, 1, 1, 3)) == 8)
+assert(waterVol(Vector(0, 3, 2, 1, 4)) == 3)
+assert(waterVol(Vector(1, 0, 1, 0)) == 1)
+assert(waterVol(Vector(1, 0, 1, 2, 0, 2)) == 3)
+assert(waterVol(Vector(2, 5, 1, 2, 3, 4, 7, 7, 6)) == 10)
+assert(waterVol(Vector(5, 1, 0, 1)) == 1)
+assert(waterVol(Vector(2, 5, 1, 2, 3, 4, 7, 7, 6, 3, 5)) == 12)
+assert(waterVol(Vector(3, 0, 1, 0, 2)) == 5)
 
