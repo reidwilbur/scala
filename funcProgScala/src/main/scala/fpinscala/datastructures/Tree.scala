b3ccd2890cfc60410c304958e4879b42ea50da37
Reid Wilbur <reid.wilbur@gmail.com>
2014-06-12 16:56:41 -0500

    ch3 impls

--- funcProgScala/src/main/scala/fpinscala/datastructures/Tree.scala
+++ funcProgScala/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,37 +7,40 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 object Tree {
 
   def size[A](t: Tree[A]): Int = 
-    t match {
-      case Leaf(_) => 1
-      case Branch(l, r) => 
-        1 + size(l) + size(r)
-    }
+    //t match {
+    //  case Leaf(_) => 1
+    //  case Branch(l, r) => 
+    //    1 + size(l) + size(r)
+    //}
+    fold(t)(_ => 1)((lc, rc) => 1 + lc + rc)
 
   def maxIntValue(t: Tree[Int]): Int =  
-    t match {
-      case Leaf(n) => n
-      case Branch(l, r) => 
-        maxIntValue(l).max(maxIntValue(r))
-    }
+    //t match {
+    //  case Leaf(n) => n
+    //  case Branch(l, r) => 
+    //    maxIntValue(l).max(maxIntValue(r))
+    //}
+    fold(t)((n) => n)((ln, rn) => ln.max(rn))
 
   def depth[A](t: Tree[A]): Int = 
-    t match {
-       case Leaf(_) => 0
-       case Branch(l, r) =>  
-         depth(l).max(depth(r)) + 1
-    }
+    //t match {
+    //   case Leaf(_) => 0
+    //   case Branch(l, r) =>  
+    //     depth(l).max(depth(r)) + 1
+    //}
+    fold(t)(_ => 0)((ld, rd) => ld.max(rd) + 1)
 
   def map[A,B](t: Tree[A])(f: (A) => B): Tree[B] = 
-    t match {
-      case Leaf(a) => Leaf(f(a))
-      case Branch(l, r) => 
-        Branch(map(l)(f), map(r)(f))
-    }
+    //t match {
+    //  case Leaf(a) => Leaf(f(a))
+    //  case Branch(l, r) => 
+    //    Branch(map(l)(f), map(r)(f))
+    //}
+    fold(t)( { a => Leaf(f(a)): Tree[B] } )( (lb, rb) => Branch(lb, rb) )
     
-//  def fold[A,B](t: Tree[A], z: B)(f: (A, B) => B): B =
-//    t match {
-//      case l: Leaf => f(l, z)
-//      case b: Branch => 
-//        
-//    }
+  def fold[A,B](t: Tree[A])(l: (A) => B)(b: (B,B) => B): B = 
+    t match {
+      case leaf: Leaf[A] => l(leaf.value)
+      case branch: Branch[A] => b(fold(branch.left)(l)(b), fold(branch.right)(l)(b))
+    }
 }
