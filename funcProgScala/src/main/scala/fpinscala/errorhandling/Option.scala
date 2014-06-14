1837b22287020161e28cfd21abef35ebf9c78cc5
Reid Wilbur <reid.wilbur@gmail.com>
2014-06-14 13:56:50 -0500

    ch4 ex 1

--- funcProgScala/src/main/scala/fpinscala/errorhandling/Option.scala
+++ funcProgScala/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -1,42 +1,33 @@
 package fpinscala.errorhandling
 
-sealed trait MyOption[+A] {
-  def map[B](f: A => B): MyOption[B] = 
+sealed trait OOption[+A] {
+  def map[B](f: A => B): OOption[B] = 
     this match {
-      case MySome(a) => MySome(f(a))
-      case _ => MyNone
+      case SSome(a) => SSome(f(a))
+      case _ => NNone
     }
 
-  def flatMap[B](f: A => MyOption[B]): MyOption[B] = 
-    this match {
-      case MySome(a) => f(a)
-      case _ => MyNone
-    }
-    //map(f).map(ob => ob.get)
-    
   def getOrElse[B >: A](default: => B): B = 
     this match {
-      case MySome(a) => a
+      case SSome(a) => a
       case _ => default
     }
 
-  def orElse[B >: A](ob: => MyOption[B]): MyOption[B] = {
-    this match {
-      case MySome(a) => this
-      case _ => ob
-    }
-    //flatMap(a => this).getOrElse(orelse)
-  }
+  def flatMap[B](f: A => OOption[B]): OOption[B] = 
+    map(f).getOrElse(NNone)
+    
+  def orElse[B >: A](ob: => OOption[B]): OOption[B] = 
+    map(a => this).getOrElse(ob)
     
 
-  def filter(f: A => Boolean): MyOption[A] = 
-    flatMap(a => if (f(a)) MySome(a) else MyNone)
+  def filter(f: A => Boolean): OOption[A] = 
+    flatMap(a => if (f(a)) SSome(a) else NNone)
 }
-case class MySome[+A](get: A) extends MyOption[A]
-case object MyNone extends MyOption[Nothing]
+case class SSome[+A](get: A) extends OOption[A]
+case object NNone extends OOption[Nothing]
 
 
-object MyOption {
+object OOption {
   def failingFn(i: Int): Int = {
     val y: Int = throw new Exception("fail!") // `val y: Int = ...` declares `y` as having type `Int`, and sets it equal to the right hand side of the `=`.
     try {
@@ -54,7 +45,7 @@ object MyOption {
     catch { case e: Exception => 43 }
   }
 
-  def mean(xs: Seq[Double]): MyOption[Double] =
-    if (xs.isEmpty) MyNone
-    else MySome(xs.sum / xs.length)
+  def mean(xs: Seq[Double]): OOption[Double] =
+    if (xs.isEmpty) NNone
+    else SSome(xs.sum / xs.length)
 }
--- funcProgScala/src/test/scala/fpinscala/errorhandling/OptionSuite.scala
+++ funcProgScala/src/test/scala/fpinscala/errorhandling/OptionSuite.scala
@@ -6,43 +6,43 @@ import org.junit.runner.RunWith
 
 @RunWith(classOf[JUnitRunner])
 class OptionSuite extends FunSuite {
-  test("MyOption.map returns correct value") {
+  test("OOption.map returns correct value") {
     
-    assert(MySome(2) == MySome(1).map(i => i+1))
+    assert(SSome(2) == SSome(1).map(i => i+1))
 
-    assert(MyNone == MyNone.map(_.toString))
+    assert(NNone == NNone.map(_.toString))
   }
 
-  test("MyOption.flatMap returns correct value") {
+  test("OOption.flatMap returns correct value") {
 
-    def plusOneIfLtThree(i: Int): MyOption[Int] = {
-      if (i < 3) MySome(i+1) else MyNone
+    def plusOneIfLtThree(i: Int): OOption[Int] = {
+      if (i < 3) SSome(i+1) else NNone
     }
 
-    assert(MySome(2) == MySome(1).flatMap(plusOneIfLtThree))
-    assert(MyNone == MySome(3).flatMap(plusOneIfLtThree))
-    assert(MyNone == MyNone.flatMap(plusOneIfLtThree))
+    assert(SSome(2) == SSome(1).flatMap(plusOneIfLtThree))
+    assert(NNone == SSome(3).flatMap(plusOneIfLtThree))
+    assert(NNone == NNone.flatMap(plusOneIfLtThree))
   }
 
-  test("MyOption.getOrElse returns correct value") {
+  test("OOption.getOrElse returns correct value") {
 
-    assert(2 == MySome(2).getOrElse(0))
-    assert(0 == MyNone.getOrElse(0))
+    assert(2 == SSome(2).getOrElse(0))
+    assert(0 == NNone.getOrElse(0))
   }
 
-  test("MyOption.orElse returns correct value") {
+  test("OOption.orElse returns correct value") {
 
-    assert(MySome(2) == MySome(2).orElse(MySome(3)))
-    assert(MySome(3) == MyNone.orElse(MySome(3)))
-    assert(MyNone == MyNone.orElse(MyNone))
+    assert(SSome(2) == SSome(2).orElse(SSome(3)))
+    assert(SSome(3) == NNone.orElse(SSome(3)))
+    assert(NNone == NNone.orElse(NNone))
   }
 
-  test("MyOption.filter returns correct value") {
+  test("OOption.filter returns correct value") {
 
     def fltr(i: Int): Boolean = i < 3
 
-    assert(MySome(2) == MySome(2).filter(fltr))
-    assert(MyNone == MySome(3).filter(fltr))
-    assert(MyNone == MyNone.filter(fltr))
+    assert(SSome(2) == SSome(2).filter(fltr))
+    assert(NNone == SSome(3).filter(fltr))
+    assert(NNone == NNone.filter(fltr))
   }
 }
