0ac652e98ff4e76b7b863bfdcfb3608b23bff458
Reid Wilbur <reid.wilbur@gmail.com>
2014-10-13 21:15:20 -0400

    Updated idea workspace and re-implemented ch 6.1

--- funcProgScala/src/main/scala/fpinscala/state/State.scala
+++ funcProgScala/src/main/scala/fpinscala/state/State.scala
@@ -5,19 +5,9 @@ trait RNG {
   def nextInt: (Int, RNG) // Should generate a random `Int`. We'll later define other functions in terms of `nextInt`.
 }
 
-case class State[S,+A](run: S => (A,S)) {
-  def map[B](f: A => B): State[S,B] =
-    flatMap(a => State.unit(f(a)))
-  def map2[B,C](s: State[S,B])(f: (A,B) => C): State[S,C] =
-    flatMap(a => s.map{ b => f(a,b) })
-  def flatMap[B](g: A => State[S,B]): State[S,B] = 
-    State(s => {
-      val (a, s1) = run(s)
-      g(a).run(s1)
-    })
-}
-
 object RNG {
+  // NB - this was called SimpleRNG in the book text
+
   case class Simple(seed: Long) extends RNG {
     def nextInt: (Int, RNG) = {
       val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL // `&` is bitwise AND. We use the current seed to generate a new seed.
@@ -25,7 +15,6 @@ object RNG {
       val n = (newSeed >>> 16).toInt // `>>>` is right binary shift with zero fill. The value `n` is our new pseudo-random integer.
       (n, nextRNG) // The return value is a tuple containing both a pseudo-random integer and the next `RNG` state.
     }
-
   }
 
   type Rand[+A] = RNG => (A, RNG)
@@ -36,90 +25,49 @@ object RNG {
     rng => (a, rng)
 
   def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
-    //rng => {
-    //  val (a, rng2) = s(rng)
-    //  (f(a), rng2)
-    //}
-    flatMap(s){ a => rng => (f(a), rng) }
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
 
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (i, rng2) = rng.nextInt
-    (if (i < 0) -(i + 1) else i, rng2)
+    if (i < 0) (-1*(i+1), rng2) else (i, rng2);
   }
 
-  def double: Rand[Double] = 
-    map(nonNegativeInt)(i => i.toDouble/(Int.MaxValue.toDouble+1))
+  def double(rng: RNG): (Double, RNG) = ???
 
-  def intDouble: Rand[(Int, Double)] = 
-    map2(int, double)((_, _))
+  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
 
-  def doubleInt: Rand[(Double, Int)] = 
-    map2(double, int)((_, _))
+  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
 
-  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
-    val (d1, rng2) = double(rng)
-    val (d2, rng3) = double(rng2)
-    val (d3, rng4) = double(rng3)
+  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
 
-    ((d1,d2,d3), rng4)
-  }
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
 
-  def ints(count: Int): Rand[List[Int]] = 
-  //(rng: RNG): (List[Int], RNG) = 
-  //{
-  //  def genInts(ccount: Int, l: List[Int], rng: RNG): (List[Int], RNG)  = 
-  //    if (ccount == 0) (l, rng) 
-  //    else {
-  //      val (i, nrng) = rng.nextInt
-  //      genInts(ccount-1, i :: l, nrng)
-  //    }
-  //  genInts(count, Nil, rng)
-  //}
-    sequence(List.fill(count)(int))
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
 
-  def nonNegativeEven: Rand[Int] =
-    map(nonNegativeInt)(i => i - i % 2)
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A,B) => C): Rand[C] = 
-    //rng => {
-    //  val (a, arng) = ra(rng)
-    //  val (b, brng) = rb(arng)
-    //  (f(a,b), brng)
-    //}
-    flatMap(ra){ a => map(rb){ b => f(a,b) } }
-   
-  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] = 
-    map2(ra, rb)((_, _))
-
-  def sequence[A](ra: List[Rand[A]]): Rand[List[A]] = 
-    rng => {
-      ra.foldRight((Nil:List[A], rng)){ 
-        (rnd, l_rng) => 
-          val (a, rng1) = rnd(l_rng._2)
-          (a :: l_rng._1, rng1)
-      }
-    }
-
-  def nonNegativeLessThan(n: Int): Rand[Int] = {
-    flatMap(nonNegativeInt){ i: Int =>
-      val mod = i % n
-      if (i + (n-1) - mod >= 0)
-        rng => (mod, rng)
-      else
-        nonNegativeLessThan(n)
-    }
-  }
-
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = 
-    rng => {
-      val (a, rng1) = f(rng)
-      g(a)(rng1)
-    }
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
 }
+
+case class State[S,+A](run: S => (A, S)) {
+  def map[B](f: A => B): State[S, B] =
+    sys.error("todo")
+  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    sys.error("todo")
+  def flatMap[B](f: A => State[S, B]): State[S, B] =
+    sys.error("todo")
+}
+
+sealed trait Input
+case object Coin extends Input
+case object Turn extends Input
+
+case class Machine(locked: Boolean, candies: Int, coins: Int)
 
 object State {
   type Rand[A] = State[RNG, A]
-
-  def unit[S,A](a: A): State[S,A] =
-    State(s => (a,s))
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
 }
--- funcProgScala/src/test/scala/fpinscala/state/StateSuite.scala
+++ funcProgScala/src/test/scala/fpinscala/state/StateSuite.scala
@@ -6,67 +6,21 @@ import org.junit.runner.RunWith
 
 @RunWith(classOf[JUnitRunner])
 class StateSuite extends FunSuite {
-  test("1 RNG.nonNegativeInt returns correct value") {
-    val (nni, _) = RNG.nonNegativeInt(RNG.Simple(42))
-    assert(nni > 0)
+  class ConstantRNG(value: Int) extends RNG {
+    override def nextInt: (Int, RNG) = {
+      (value, this)
+    }
   }
 
-  test("2 RNG.double returns correct value") {
-    val (d, _) = RNG.double(RNG.Simple(42))
-    assert(d >= 0.0)
-    assert(d < 1.0)
+  test("nonNegative should return Integer.MAX_VALUE instead Integer.MIN_VALUE") {
+    assert(Integer.MAX_VALUE == RNG.nonNegativeInt(new ConstantRNG(Integer.MIN_VALUE))._1)
   }
 
-  test("nonNegativeEven returns correct value") {
-    val rand = RNG.nonNegativeEven(RNG.Simple(42))
-    val v = rand._1
-    assert(v % 2 == 0)
-    assert(v > 0)
+  test("nonNegative should return 0 instead of -1") {
+    assert(0 == RNG.nonNegativeInt(new ConstantRNG(-1))._1)
   }
 
-  test("5 RNG.double returns correct value") {
-    val rand = RNG.double(RNG.Simple(42))
-    val v = rand._1
-    assert(v > 0.0)
-    assert(v < 1.0)
-  }
-
-  test("6 RNG.map2 returns correct value") {
-    val id_rnd = RNG.map2(RNG.nonNegativeInt, RNG.double)((i,d) => (i,d))(RNG.Simple(42))
-    assert(id_rnd._1._1 > 0)
-    assert(id_rnd._1._2 >= 0.0)
-    assert(id_rnd._1._2 < 1.0)
-  }
-
-  test("RNG.intDouble returns correct value") {
-    val id_rnd = RNG.intDouble(RNG.Simple(42))
-    assert(id_rnd._1._2 >= 0.0)
-    assert(id_rnd._1._2 < 1.0)
-  }
-
-  test("RNG.doubleInt returns correct value") {
-    val id_rnd = RNG.doubleInt(RNG.Simple(42))
-    assert(id_rnd._1._1 >= 0.0)
-    assert(id_rnd._1._1 < 1.0)
-  }
-
-  test("7 RNG.sequence returns correct value") {
-    val rndList = RNG.sequence( List.fill(10)(RNG.nonNegativeEven) )(RNG.Simple(42))
-    assert(rndList._1.length == 10)
-    assert(rndList._1.forall( i => (i >= 0) && (i % 2 == 0)))
-  }
-  
-  test("7 RNG.ints returns correct value") {
-    val rndList: (List[Int], RNG) = RNG.ints(10)(RNG.Simple(42))
-    assert(rndList._1.length == 10)
-  }
-
-  test("10 RNG.nonNegativeLessThan returns correct value") {
-    val rng = RNG.Simple(42)
-    for( i <- 1 to 100 )
-      {
-        val (i , r) = RNG.nonNegativeLessThan(10)(rng)
-        assert( i < 10 && i >= 0)
-      }
+  test("nonNegative should add 1 and invert negative input") {
+    assert(25 == RNG.nonNegativeInt(new ConstantRNG(-26))._1)
   }
 }
